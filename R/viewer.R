c25e5db125fefb8e1b91d3dabc1b2fef919f8bf8
Ellpeck <me@ellpeck.de>
2024-07-18 13:43:44 +0200

    generify display_html for future use

--- R/viewer.R
+++ R/viewer.R
@@ -1,4 +1,4 @@
-display_code <- function(code) {
+display_html <- function(head, body) {
   html <- sprintf('
 <!DOCTYPE html>
 <html lang="en">
@@ -6,25 +6,31 @@ display_code <- function(code) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+    %s
   </head>
   <body>
-    <div>
-      <pre>
-        <code class="language-r">
-%s
-        </code>
-      </pre>
-    </div>
-    <script>
-      hljs.highlightAll();
-    </script>
+    %s
   </body>
-</html>', code)
+</html>', head, body)
 
   file <- tempfile(fileext = ".html")
   print(paste0("Using temp file ", file))
   writeLines(html, file)
   getOption("viewer")(file)
+}
+
+display_code <- function(code) {
+  display_html('
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>', sprintf('
+<div>
+  <pre>
+    <code class="language-r">
+%s
+    </code>
+  </pre>
+</div>
+<script>
+  hljs.highlightAll();
+</script>', code))
 }
