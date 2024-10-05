e1c4eadb53f5c0dc5e22ed702c09e32632773e22
Ellpeck <me@ellpeck.de>
2024-10-05 17:44:07 +0200

    allow using docker instead of local node installation

--- R/shell.R
+++ R/shell.R
@@ -18,16 +18,28 @@ make_flowr_session_storage <- function() {
     # connect if we're not connected yet
     if (is.null(connection)) {
       if (read_flowr_pref(pref_use_local_shell, default_use_local_shell)) {
-        # start the shell
-        pid <<- tryCatch(
-          flowr::exec_flowr(c("--server", "--port", default_server_port), TRUE, node_base_dir(), TRUE),
-          error = function(e) {
-            message(paste0("Failed to start local flowR server: ", e, "If flowR is not installed, you can do so by running the \"Install Node.js and flowR Shell\" addin (rstudioaddinflowr:::install_node_addin()). Do you want to to do so right away?"))
-            if(rstudioapi::showQuestion("Install Node.js and flowR Shell?", "The preferences state that you want to use a local flowR installation, but it is not installed. Do you want to install it now?")) {
-              install_node_addin()
+        args <- c("--server", "--port", default_server_port)
+        if (!read_flowr_pref(pref_use_docker, default_use_docker)) {
+          # start the shell using node
+          pid <<- tryCatch(
+            flowr::exec_flowr(args, TRUE, node_base_dir(), TRUE),
+            error = function(e) {
+              message(paste0("Failed to start local flowR server: ", e, "If flowR is not installed, you can do so by running the \"Install Node.js and flowR Shell\" addin (rstudioaddinflowr:::install_node_addin()). Do you want to to do so right away?"))
+              if (rstudioapi::showQuestion("Install Node.js and flowR Shell?", "The preferences state that you want to use a local flowR installation, but it is not installed. Do you want to install it now?")) {
+                install_node_addin()
+              }
             }
-          }
-        )
+          )
+        } else {
+          # start the shell using docker
+          flowr_ver <- read_flowr_pref(pref_flowr_version, default_flowr_version)
+          pid <<- tryCatch(
+            flowr::exec_flowr_docker(c("-p", paste0(default_server_port, ":", default_server_port)), flowr_ver, args, TRUE, "docker", TRUE),
+            error = function(e) {
+              message(paste0("Failed to start local flowR server using Docker: ", e, "If you want to run flowR using a local noe installation instead, you can change your preferences using the \"Open Preferences\" addin (rstudioaddinflowr:::open_prefs_addin())."))
+            }
+          )
+        }
         print(paste0("Starting local flowR server with pid ", pid))
         host <- default_server_host
         port <- default_server_port
